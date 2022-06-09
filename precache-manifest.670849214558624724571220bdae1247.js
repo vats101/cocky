dc630f522373dc3edc9bca73fe357aca99a3e852
Vatsal Mehta <vatsalmehta103@gmail.com>
2022-06-09 20:23:26 +0530

    Updates

---
+++ precache-manifest.670849214558624724571220bdae1247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d5b6fd81db2d28dafb82bd71314193",
+    "url": "/cocky/index.html"
+  },
+  {
+    "revision": "2f247f0cfb0acb61226e",
+    "url": "/cocky/static/css/main.f2e69952.chunk.css"
+  },
+  {
+    "revision": "f1e4852a6741289f00ae",
+    "url": "/cocky/static/js/2.653d3665.chunk.js"
+  },
+  {
+    "revision": "adebb55e52d2637432a3b77edb340c7f",
+    "url": "/cocky/static/js/2.653d3665.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f247f0cfb0acb61226e",
+    "url": "/cocky/static/js/main.e8892d6e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/cocky/static/js/main.e8892d6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "376651aa3624a910f9d0",
+    "url": "/cocky/static/js/runtime-main.f455adbc.js"
+  }
+]);
